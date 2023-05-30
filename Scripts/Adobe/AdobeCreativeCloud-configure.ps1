f5d494219b38dc9f2d6e69fce4f3fd2197eef9cf
Sinderan <sinderan@outlook.com>
2023-05-30 15:01:10 -0400

    Created Adobe Creative Cloud configure

---
+++ Scripts/Adobe/AdobeCreativeCloud-configure.ps1
@@ -0,0 +1,20 @@
+$Path = "$env:Programdata\Adobe\OperatingConfigs"
+$File = "TOOLKIT.CONFIG"
+
+Write-Output "Checking if exists: $Path"
+If(!(Test-Path -PathType Container $Path))
+{
+    Write-Output "Path not found, Creating Path"
+    New-Item -ItemType Directory -Path $Path
+} Else {
+    Write-Output "Path Exists"
+}
+
+Write-Output "Checking if exists: $Path\$File"
+If(!(Test-Path -PathType Leaf $Path\$File))
+{
+    Write-Output "Path not found, Creating File"
+    Copy-Item -Path $File -Destination $Path
+} Else {
+    Write-Output "File Exists"
+}
